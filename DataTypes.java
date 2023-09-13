e76e4cb8c8977c9d984816b46c7f62476fc6b344
Francisco Saroldi-Gonzalez <francisco.saroldi-gonzalez@mrjaffesclass.com>
2023-09-13 11:44:49 -0700

    arrays done some

--- DataTypes.java
+++ DataTypes.java
@@ -4,12 +4,14 @@ public class DataTypes
         int i;
         double f = 2.5;
         boolean flag = true;
+        
         i = 10;
         f += i;
         flag = !flag;
-        System.out.print(++i);
-        System.out.print(f);
-        System.out.print(flag);
+        System.out.println(++i);
+        System.out.println(f);
+        System.out.println(flag);
+        System.out.println(" ");
         String String1 = "This is a string";
         String String2 = String1;
         String String3 = "I am also a string";
@@ -18,5 +20,11 @@ public class DataTypes
         System.out.println(String2);
         System.out.println(String3);
         System.out.println(stringsEqual);
+        
+        String[] strArray = {"Alpha:","Bravo","Charlie","Delta","Echo"};
+        for  (int iterate = 0; iterate < strArray.length; iterate++) {
+            System.out.println(strArray[iterate]);
+        }
+        
         }
     }
